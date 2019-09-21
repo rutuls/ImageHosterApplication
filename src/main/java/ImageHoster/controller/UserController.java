ba6da6a8e1cb0befc4a5162e877facd506c7719c
rutuls <rutulshah2007@gmail.com>
2019-09-21 14:08:21 +0530

    Adding new feature for validating password strength before registering

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 @Controller
@@ -39,10 +41,61 @@ public class UserController {
 
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
+    
+    /*Before registering user the valid password check has been added. Register user only if user password contains at least 1 alphabet, 1 number and 1 special character*/
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
-        userService.registerUser(user);
-        return "redirect:/users/login";
+    public String registerUser(User user, Model model) {
+        String passwordTypeError = "Password must contain at least 1 alphabet, 1 number & 1 special character";
+        String password = user.getPassword();
+        if (isValidPassword(password)) {
+          userService.registerUser(user);
+          return "redirect:/users/login";
+        } else {
+          User inValidUser = new User();
+          model.addAttribute("User", inValidUser);
+          model.addAttribute("passwordTypeError", passwordTypeError);
+          return "users/registration";
+        }
+    }
+    
+    /* isValidPassword method checks the entered password contains atleast one character and number and digit.
+     * Implementation Strategy/Logic :
+     * Run for loop till the length of password string character by character
+     * Get the current character and check if it is alphabetic or digit or special character
+     * isAlphabetic checks if current character is alphabetic
+     * isDigit checks if current character is digit
+     * isSpecialCharPresent checks if current character is special character. Here we are calling another function to check that by regex. 
+     */
+    public boolean isValidPassword(String password) {
+      boolean characterFlag = false;
+      boolean numberFlag = false;
+      boolean specialCharFlag = false;
+      for(int i = 0; i < password.length(); i++) {
+        char currentChar = password.charAt(i);
+        if (Character.isAlphabetic(currentChar)) {
+          characterFlag = true;
+        } else if (Character.isDigit(currentChar)) {
+          numberFlag = true;
+        }
+      }
+      if (isSpecialCharPresent(password)) {
+        specialCharFlag = true;
+      }
+      if (characterFlag && numberFlag && specialCharFlag) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    
+    /* This method uses pattern and matcher library to verify regular expression which verifies it is not an alphabetic and digit.
+     * [^A-Za-z0-9] regular expression checks for string which does not contain any alphabetic and digits
+     */    
+    public boolean isSpecialCharPresent(String password) {
+      Pattern pattern = Pattern.compile("[^A-Za-z0-9]");
+      Matcher matcher = pattern.matcher(password);
+      boolean specialCharFlag = matcher.find();
+      return specialCharFlag;
     }
 
     //This controller method is called when the request pattern is of type 'users/login'
